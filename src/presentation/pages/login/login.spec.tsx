6edb31e21859c6a99fce2ddef4244fd417d4364b
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-10-22 07:08:34 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -142,4 +142,12 @@ describe('Login Component', () => {
       password
     })
   })
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
 })
