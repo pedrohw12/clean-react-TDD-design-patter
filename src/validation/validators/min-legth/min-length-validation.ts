9b281642bc67ccb00721d1d53889f034ebd9178e
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-10-26 11:58:44 -0300

    feat: ensure MinLenthValidation returns falsy if value is valid

--- src/validation/validators/min-legth/min-length-validation.ts
+++ src/validation/validators/min-legth/min-length-validation.ts
@@ -1,10 +1,10 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
-export
-class MinLengthValidation implements FieldValidation {
+export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
+
   validate (value: string): Error {
-    return new InvalidFieldError()
+    return value.length >= this.minLength ? null : new InvalidFieldError()
   }
 }
