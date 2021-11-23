f7e4353193927eca052a215bb0c03c9cd5059a95
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-11-23 16:48:59 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -10,14 +12,11 @@ describe('Login', () => {
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
-
     cy.getByTestId('password').should('have.attr', 'readOnly')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
-
     cy.getByTestId('submit').should('have.attr', 'disabled')
-
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
@@ -26,14 +25,11 @@ describe('Login', () => {
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
-
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
-
     cy.getByTestId('submit').should('have.attr', 'disabled')
-
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
@@ -42,14 +38,23 @@ describe('Login', () => {
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Tudo certo!')
       .should('contain.text', '🟢')
-
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Tudo certo!')
       .should('contain.text', '🟢')
-
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
-
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('exist')
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 })
