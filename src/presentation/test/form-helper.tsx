f9f2cf3f97aaa13bc68c59333924001296b955ac
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-11-26 10:34:03 -0300

    refactor: change asserts to use jest-dom

--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -1,20 +1,6 @@
 import faker from 'faker'
 import { fireEvent, screen } from '@testing-library/react'
 
-export const testChildCount = (fieldName: string, count: number): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el.childElementCount).toBe(count)
-}
-
-export const testButtonIsDisabled = (
-  fieldName: string,
-  isDisabled: boolean
-): void => {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  const button = screen.getByTestId(fieldName) as HTMLButtonElement
-  expect(button.disabled).toBe(isDisabled)
-}
-
 export const testStatusForField = (
   fieldName: string,
   validationError: string = ''
@@ -22,11 +8,12 @@ export const testStatusForField = (
   const wrap = screen.getByTestId(`${fieldName}-wrap`)
   const field = screen.getByTestId(fieldName)
   const label = screen.getByTestId(`${fieldName}-label`)
-  expect(wrap.getAttribute('data-status')).toBe(
+  expect(wrap).toHaveAttribute(
+    'data-status',
     validationError ? 'invalid' : 'valid'
   )
-  expect(field.title).toBe(validationError)
-  expect(label.title).toBe(validationError)
+  expect(field).toHaveProperty('title', validationError)
+  expect(label).toHaveProperty('title', validationError)
 }
 
 export const populateField = (
@@ -37,14 +24,4 @@ export const populateField = (
   fireEvent.input(input, {
     target: { value }
   })
-}
-
-export const testElementExists = (fieldName: string): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el).toBeTruthy()
-}
-
-export const testElementText = (fieldName: string, text: string): void => {
-  const el = screen.getByTestId(fieldName)
-  expect(el.textContent).toBe(text)
 }
