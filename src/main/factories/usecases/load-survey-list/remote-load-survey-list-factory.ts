8bf257a3972828b09648dbc63a5e237a88722974
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-11-30 16:42:49 -0300

    feat: add makeAuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,11 +1,11 @@
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
 import { makeApiUrl } from '@/main/factories/http/api-url-factory';
 import { LoadSurveyList } from '@/domain/usecases';
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list';
+import { makeAuthorizeHttpGetClientDecorator } from '../../decorators';
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
   return new RemoteLoadSurveyList(
     makeApiUrl('/surveys'),
-    makeAxiosHttpClient()
+    makeAuthorizeHttpGetClientDecorator()
   );
 };
