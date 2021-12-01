7bc6d71b1c1d893d57fe7d67c97ddd9f4b18cc0a
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-01 18:14:33 -0300

    test: ensure LocalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,37 +1,42 @@
-import { LocalStorageAdapter } from './local-storage-adapter'
+import { LocalStorageAdapter } from './local-storage-adapter';
 
-import faker from 'faker'
-import 'jest-localstorage-mock'
+import faker from 'faker';
+import 'jest-localstorage-mock';
 
-const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter();
 
 describe('LocalStorageAdapter', () => {
   beforeEach(() => {
-    localStorage.clear()
-  })
+    localStorage.clear();
+  });
 
   test('Should call localstorage.setItem with correct values', () => {
-    const sut = makeSut()
-    const key = faker.database.column()
-    const value = faker.random.objectElement<{}>()
-
-    sut.set(key, value)
-
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.objectElement<{}>();
+    sut.set(key, value);
     expect(localStorage.setItem).toHaveBeenCalledWith(
       key,
       JSON.stringify(value)
-    )
-  })
+    );
+  });
+
+  test('Should call localstorage.removeItem if value is null', () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    sut.set(key, undefined);
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key);
+  });
 
   test('Should call localstorage.getItem with correct value', () => {
-    const sut = makeSut()
-    const key = faker.database.column()
-    const value = faker.random.objectElement<{}>()
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.objectElement<{}>();
     const getItemSpy = jest
       .spyOn(localStorage, 'getItem')
-      .mockReturnValueOnce(JSON.stringify(value))
-    const obj = sut.get(key)
-    expect(obj).toEqual(value)
-    expect(getItemSpy).toHaveBeenCalledWith(key)
-  })
-})
+      .mockReturnValueOnce(JSON.stringify(value));
+    const obj = sut.get(key);
+    expect(obj).toEqual(value);
+    expect(getItemSpy).toHaveBeenCalledWith(key);
+  });
+});
