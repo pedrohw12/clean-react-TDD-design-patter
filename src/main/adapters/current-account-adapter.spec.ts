896c99e60eeb9631a34213962dc735f4f5fd01fa
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-01 18:14:02 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,34 +1,27 @@
-import { UnexpectedError } from '@/domain/errors'
-import { mockAccountModel } from '@/domain/test'
-import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { mockAccountModel } from '@/domain/test';
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter';
 import {
   setCurrentAccountAdapter,
   getCurrentAccountAdapter
-} from './current-account-adapter'
+} from './current-account-adapter';
 
-jest.mock('@/infra/cache/local-storage-adapter')
+jest.mock('@/infra/cache/local-storage-adapter');
 
 describe('CurrentAccountAdapter', () => {
   test('Should call LocalStorageAdapter.set with correct values', () => {
-    const account = mockAccountModel()
-    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
-    setCurrentAccountAdapter(account)
-    expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexcpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
-  })
+    const account = mockAccountModel();
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set');
+    setCurrentAccountAdapter(account);
+    expect(setSpy).toHaveBeenCalledWith('account', account);
+  });
 
   test('Should call LocalStorageAdapter.get with correct value', () => {
-    const account = mockAccountModel()
+    const account = mockAccountModel();
     const getSpy = jest
       .spyOn(LocalStorageAdapter.prototype, 'get')
-      .mockReturnValueOnce(account)
-    const result = getCurrentAccountAdapter()
-    expect(getSpy).toHaveBeenCalledWith('account')
-    expect(result).toEqual(account)
-  })
-})
+      .mockReturnValueOnce(account);
+    const result = getCurrentAccountAdapter();
+    expect(getSpy).toHaveBeenCalledWith('account');
+    expect(result).toEqual(account);
+  });
+});
