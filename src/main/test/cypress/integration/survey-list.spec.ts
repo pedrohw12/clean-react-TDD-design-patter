74ff789b616fc826eae9b3e24d0f1c9cccfd1430
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-04 11:46:11 -0300

    refactor: organize integration tests with fixtures

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -1,17 +1,20 @@
-import faker from 'faker';
-import * as Http from '../support/survey-list-mocks';
-import * as Helper from '../support/helpers';
+import * as Http from '../utils/http-mocks';
+import * as Helper from '../utils/helpers';
+
+const path = /surveys/;
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET');
+
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET');
 
 describe('SurveyList', () => {
   beforeEach(() => {
-    Helper.setLocalStorageItem('account', {
-      accessToken: faker.random.uuid(),
-      name: faker.name.findName()
+    cy.fixture('account').then((account) => {
+      Helper.setLocalStorageItem('account', account);
     });
   });
 
   it('Should present error on UnexpectedError', () => {
-    Http.mockUnexpectedError();
+    mockUnexpectedError();
     cy.visit('');
     cy.getByTestId('error').should(
       'contain.text',
@@ -20,20 +23,20 @@ describe('SurveyList', () => {
   });
 
   it('Should logout on AccessDeniedError', () => {
-    Http.mockAccessDeniedError();
+    mockAccessDeniedError();
     cy.visit('');
     Helper.testUrl('/login');
   });
 
   it('Should present correct username', () => {
-    Http.mockUnexpectedError();
+    mockUnexpectedError();
     cy.visit('');
     const { name } = Helper.getLocalStorageItem('account');
     cy.getByTestId('username').should('contain.text', name);
   });
 
   it('Should logout on logout link click', () => {
-    Http.mockUnexpectedError();
+    mockUnexpectedError();
     cy.visit('');
     cy.getByTestId('logout').click();
     Helper.testUrl('/login');
