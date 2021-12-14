12ac5ce719c65fd3595fb7a65621b8703bd37542
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-14 12:06:12 -0300

    feat: add SurveyResult layout

---
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import FlipMove from 'react-flip-move';
+import { Footer, Header, Spinner } from '@/presentation/components';
+import Styles from './survey-result-styles.scss';
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Qual seu framework web favorito?</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img src="" />
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Voltar</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde...</span>
+            <Spinner isNegative />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default SurveyResult;
