8a3b39bdfc581da3b09b97c5318795fe212c0aad
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-15 17:11:03 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FlipMove from 'react-flip-move';
-import { Calendar, Footer, Header, Loading } from '@/presentation/components';
+import {
+  Calendar,
+  Footer,
+  Header,
+  Loading,
+  Error
+} from '@/presentation/components';
 import Styles from './survey-result-styles.scss';
+import { LoadSurveyResult } from '@/domain/usecases';
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model
+  });
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-          <h2>Qual seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img src="" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        <Loading />
+      <div data-testid="survey-result" className={Styles.contentWrap}>
+        {state.surveyResult && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        )}
+        {state.isLoading && <Loading />}
+        {state.error && <Error error={state.error} reload={() => null} />}
       </div>
       <Footer />
     </div>
