315ec15377cd44e902a6868f75de1b8562048ef0
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-16 18:57:20 -0300

    test: ensure SurveyResult redirects to login if no token if found

--- src/main/test/cypress/integration/private-routes.ts
+++ src/main/test/cypress/integration/private-routes.ts
@@ -1,8 +1,29 @@
-import * as Helper from '../support/helpers';
+import { PrivateRoute } from '@/main/proxies'
+import { renderWithHistory } from '@/tests/presentation/mocks'
+import { mockAccountModel } from '@/tests/domain/mocks'
 
-describe('Private Routes', () => {
-  it('Should logout if survey-list has no token', () => {
-    cy.visit('');
-    Helper.testUrl('/login');
-  });
-});
+import { createMemoryHistory, MemoryHistory } from 'history'
+
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (account = mockAccountModel()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  renderWithHistory({ history, Page: PrivateRoute, account })
+  return { history }
+}
+
+describe('PrivateRoute', () => {
+  test('Should redirect to /login if token is empty', () => {
+    const { history } = makeSut(null)
+
+    expect(history.location.pathname).toBe('/login')
+  })
+
+  test('Should render current component if token is not empty', () => {
+    const { history } = makeSut()
+
+    expect(history.location.pathname).toBe('/')
+  })
+})
