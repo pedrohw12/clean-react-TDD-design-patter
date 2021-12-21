14cc6dc2084a8c63004976d7b274ba3f412d2811
Pedro Wanderley <wanderley.pedro@outlook.com>
2021-12-21 17:36:31 -0300

    refactor: change surveyList to use recoil

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,53 +1,44 @@
-import { LoadSurveyList } from '@/domain/usecases';
-import { Footer, Header, Error } from '@/presentation/components';
-import { useErrorHandler } from '@/presentation/hooks';
-import React, { useEffect, useState } from 'react';
-import { SurveyListItem } from './components';
-import Styles from './item-styles.scss';
+import Styles from './survey-list-styles.scss'
+import { Header, Footer, Error } from '@/presentation/components'
+import { SurveyListItem, surveyListState } from '@/presentation/pages/survey-list/components'
+import { useErrorHandler } from '@/presentation/hooks'
+import { LoadSurveyList } from '@/domain/usecases'
+
+import { useRecoilState, useResetRecoilState } from 'recoil'
+import React, { useEffect } from 'react'
 
 type Props = {
-  loadSurveyList: LoadSurveyList;
-};
+  loadSurveyList: LoadSurveyList
+}
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const resetSurveyListState = useResetRecoilState(surveyListState)
   const handleError = useErrorHandler((error: Error) => {
-    setState((old) => ({ ...old, error: error.message }));
-  });
-  const [state, setState] = useState({
-    surveys: [] as LoadSurveyList.Model[],
-    error: '',
-    reload: false
-  });
+    setState(old => ({ ...old, error: error.message }))
+  })
+  const [state, setState] = useRecoilState(surveyListState)
+  const reload = (): void => setState(old => ({ surveys: [], error: '', reload: !old.reload }))
 
-  const reload = (): void => {
-    setState((old) => ({
-      surveys: [],
-      error: '',
-      reload: !old.reload
-    }));
-  };
-
+  useEffect(() => resetSurveyListState(), [])
   useEffect(() => {
-    loadSurveyList
-      .loadAll()
-      .then((surveys) => setState((old) => ({ ...old, surveys })))
-      .catch(handleError);
-  }, [state.reload]);
+    loadSurveyList.loadAll()
+      .then(surveys => setState(old => ({ ...old, surveys })))
+      .catch(handleError)
+  }, [state.reload])
 
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        {state.error ? (
-          <Error error={state.error} reload={reload} />
-        ) : (
-          <SurveyListItem surveys={state.surveys} />
-        )}
+        {state.error
+          ? <Error error={state.error} reload={reload} />
+          : <SurveyListItem surveys={state.surveys} />
+        }
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default SurveyList;
+export default SurveyList
